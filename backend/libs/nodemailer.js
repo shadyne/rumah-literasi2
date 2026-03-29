a65cf80763d75b708713b770fb0d571345580ff0
hady <nurhadyr2@gmai.com>
2026-03-29 20:37:12 +0700

    update nodemail

--- backend/libs/nodemailer.js
+++ backend/libs/nodemailer.js
@@ -1,12 +1,13 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-	host: process.env.NODEMAILER_HOST,
-	port: Number(process.env.NODEMAILER_PORT),
-	auth: {
-		user: process.env.NODEMAILER_USER,
-		pass: process.env.NODEMAILER_PASS,
-	},
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
 });
 
 module.exports = transporter;
